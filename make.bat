c3d86f2cb0fba581e871a51bdac4fa85525a13c4
jchristof <jchristofmartin@gmail.com>
2019-10-13 09:10:44 -0600

    adding some build automation in vscode + auto loading via -watch in bgb when rom changes

--- make.bat
+++ make.bat
@@ -1,2 +1,7 @@
 d:\gbdk-2.95-3-win32\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
 d:\gbdk-2.95-3-win32\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o main.gb main.o
+
+QPROCESS "bgb.exe">NUL
+IF %ERRORLEVEL% EQU 0 goto end
+D:\bgb\bgb -rom main.gb -watch
+:end
